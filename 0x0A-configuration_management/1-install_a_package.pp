b861cba817f3984f23d16f0dfff0fcce616898fa
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2023-03-27 14:58:52 -0700

    kill2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask -v 2.1.0
+# Installs puppet-lint, version 2.1.0
 
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+package { 'puppet-lint':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
